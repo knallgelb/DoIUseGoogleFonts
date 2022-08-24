e1ed0bd2887af270482c19d4427a9e0f1a1040ef
Rainer Amler <rainer@amler.at>
2022-08-24 09:17:46 +0200

    follow_redirects = True, added BS4 dependency

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import httpx
 import click
 import re
+from bs4 import BeautifulSoup
 
 
 @click.command()
 @click.argument('url')
 def check_url(url: str) -> str:
-    website = httpx.get(url)
+    website = httpx.get(url, follow_redirects=True)
 
     click.echo(f"Checking URL {url} now.")
 
     match = re.search(r"fonts.googleapis.com", string=website.text)
     if match:
         click.echo(f"The Website {url} contains Google-Fonts. Change that now!")
+        soup = BeautifulSoup(website.content, "html.parser")
+        click.echo(soup)
     else:
         click.echo(f"The Website {url} ist Google-Fonts free.")
 
