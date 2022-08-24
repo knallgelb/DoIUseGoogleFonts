b64592779002aa04339573621dc94bb8db96dcb4
Rainer Amler <rainer@amler.at>
2022-08-24 11:43:08 +0200

    save all files (css and fonts) to domain-folder

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ import httpx
 import click
 import re
 from bs4 import BeautifulSoup
+import os
 
 headers = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0"}
+
+
+def save_file(filename: str, filecontent: str, directory: str) -> bool:
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+    with open(os.path.join(directory, filename), mode="wb") as file:
+        file.write(filecontent)
+    return True
 
 
 @click.command()
@@ -11,6 +20,7 @@ headers = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/201001
 def check_url(url: str) -> str:
     website = httpx.get(url, follow_redirects=True)
 
+    website_name = re.search('https?://([A-Za-z_0-9.-]+).*', url).group(1)
     click.echo(f"Checking URL {url} now.")
 
     google_pattern = r"fonts.googleapis.com"
@@ -21,18 +31,14 @@ def check_url(url: str) -> str:
         soup = BeautifulSoup(website.content, "html.parser")
         google_fonts_url = soup.find("link", attrs={'href': re.compile(r'fonts.googleapis.com')})
         if google_fonts_url["href"].startswith("//"):
-            google_css = httpx.get(f"https:{ google_fonts_url['href']}", headers=headers)
+            google_css = httpx.get(f"https:{google_fonts_url['href']}", headers=headers)
             woff_files = re.findall(r"url\((.*?)\)", google_css.text)
-            click.echo("You can download the woff/woff2 files from here:")
             for font in woff_files:
-                click.echo(font)
-            click.echo("Here is your modified css-file:\n\n")
+                font_name = re.search(r"[\w_-]+.(woff2?|ttf)", font).group(0)
+                save_file(font_name, httpx.get(font).content, website_name)
             modified_css = re.sub(r"https:.*/", "", google_css.text)
-            click.echo(modified_css)
-
-
-
-
+            save_file("fonts.css", modified_css.encode("utf-8"), website_name)
+            click.echo(f"Saved all the necessary files to {website_name}.")
     else:
         click.echo(f"The Website {url} ist Google-Fonts free.")
 
